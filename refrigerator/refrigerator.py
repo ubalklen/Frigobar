72f57a8ef83aefbe3844a22d671ae38d5c2c04ac
ubalklen <42127323+ubalklen@users.noreply.github.com>
2023-07-28 21:19:33 -0300

    requirements file is now optional

--- refrigerator/refrigerator.py
+++ refrigerator/refrigerator.py
@@ -14,10 +14,9 @@ BAT_TEMPLATE_NO_REQ = '''powershell . '%~dp0/downloaders/download_python.ps1' -V
 def create_refrigerator(
     script_path: str,
     target_directory: str = "refrigerator",
-    requirements_file: str = "requirements.txt",
     python_version: str = "3.11.4",
+    requirements_file: str = None,
     copy_directory: bool = False,
-    use_proxy: bool = False,
 ):
     if not os.path.exists(target_directory):
         os.mkdir(target_directory)
@@ -32,7 +31,9 @@ def create_refrigerator(
     if not os.path.exists(script_path) or not os.path.isfile(script_path):
         raise Exception(f"Missing script: {script_path}")
 
-    if not os.path.exists(requirements_file) or not os.path.isfile(requirements_file):
+    if requirements_file and (
+        not os.path.exists(requirements_file) or not os.path.isfile(requirements_file)
+    ):
         raise Exception(f"Missing requirements file: {requirements_file}")
 
     # Add a copy of the script to refrigerator
@@ -44,16 +45,25 @@ def create_refrigerator(
         shutil.copytree(os.path.dirname(script_path), script_dir, dirs_exist_ok=True)
 
     # Add a copy of the requirements file to refrigerator
-    shutil.copy(requirements_file, script_dir)
+    if requirements_file:
+        shutil.copy(requirements_file, script_dir)
 
     # Add a copy of the downloaders to refrigerator
     downloaders_dir = os.path.join(os.path.dirname(__file__), "downloaders")
-    download_python_script = os.path.join(downloaders_dir, "download_python.ps1")
-    download_pip_script = os.path.join(downloaders_dir, "download_pip.ps1")
-    download_deps_script = os.path.join(downloaders_dir, "download_deps.ps1")
+    downloader_scripts = (
+        [
+            os.path.join(downloaders_dir, "download_python.ps1"),
+            os.path.join(downloaders_dir, "download_pip.ps1"),
+            os.path.join(downloaders_dir, "download_deps.ps1"),
+        ]
+        if requirements_file
+        else [
+            os.path.join(downloaders_dir, "download_python.ps1"),
+        ]
+    )
     downloaders_dir = os.path.join(target_directory, "downloaders")
     os.mkdir(downloaders_dir)
-    for script in [download_python_script, download_pip_script, download_deps_script]:
+    for script in downloader_scripts:
         if not os.path.exists(script):
             raise Exception(f"Missing script: {script}")
         shutil.copy(script, downloaders_dir)
@@ -62,19 +72,28 @@ def create_refrigerator(
     script_basename = os.path.splitext(os.path.basename(script_path))[0]
     bat_file = os.path.join(target_directory, f"{script_basename}.bat")
     with open(bat_file, "w") as f:
-        f.write(
-            BAT_TEMPLATE.format(
-                python_version=python_version,
-                target_directory=target_directory,
-                python_directory=python_directory,
-                use_proxy="$true" if use_proxy else "$false",
-                requirements_file=os.path.join(
-                    script_dir, os.path.basename(requirements_file)
-                ),
-                pip_path=os.path.join(python_directory, "Scripts", "pip.exe"),
-                script_path=os.path.join(script_dir, os.path.basename(script_path)),
+        if requirements_file:
+            f.write(
+                BAT_TEMPLATE.format(
+                    python_version=python_version,
+                    target_directory=target_directory,
+                    python_directory=python_directory,
+                    requirements_file=os.path.join(
+                        script_dir, os.path.basename(requirements_file)
+                    ),
+                    pip_path=os.path.join(python_directory, "Scripts", "pip.exe"),
+                    script_path=os.path.join(script_dir, os.path.basename(script_path)),
+                )
             )
-        )
+        else:
+            f.write(
+                BAT_TEMPLATE_NO_REQ.format(
+                    python_version=python_version,
+                    target_directory=target_directory,
+                    python_directory=python_directory,
+                    script_path=os.path.join(script_dir, os.path.basename(script_path)),
+                )
+            )
 
 
 def fill_refrigerator(refrigerator_path: str):
